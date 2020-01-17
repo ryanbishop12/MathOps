03d03cb1b8e3d9a93eb96664e443871b11344b3d
thatpersoninthemirror <ryan.bishop258@gmail.com>
2020-01-17 09:24:13 -0600

    initial commit

--- MathOps/Program.cs
+++ MathOps/Program.cs
@@ -8,5 +8,16 @@ namespace MathOps
         {
             Console.WriteLine("Hello World!");
         }
+
+        private static int Sum(params int[] input)
+        {
+            int output = 0;
+            foreach(int i in input)
+            {
+                output += i;
+            }
+            return output;
+        }
+
     }
 }
